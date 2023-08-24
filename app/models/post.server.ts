71bdaa1870632d2869faae96a8ecab6ea924f0a1
Michaux Kelley <michauxkelley@gmail.com>
2023-08-24 17:07:22 -0400

    Add post slug route

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -8,3 +8,7 @@ type Post = {
 export async function getPosts(): Promise<Array<Post>> {
   return prisma.post.findMany();
 }
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
+}
