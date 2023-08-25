d931e43d486ac3a8e1007ebe908dab0d8639c1ec
Michaux Kelley <michauxkelley@gmail.com>
2023-08-24 21:43:51 -0400

    Add html to post route

--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -1,18 +1,24 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { marked } from "marked";
 import { getPost } from "~/models/post.server";
+import invariant from "tiny-invariant";
 
 export const loader = async ({ params }: LoaderArgs) => {
-  const post = await getPost(params.slug!);
-  return json({ post });
+  invariant(params.slug, "params.slug is required");
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
+  const html = marked(post.markdown);
+  return json({ html, post });
 };
 
 export default function PostSlug() {
-  const { post } = useLoaderData<typeof loader>();
+  const { html, post } = useLoaderData<typeof loader>();
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post?.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
